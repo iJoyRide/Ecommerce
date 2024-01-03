7b8e577ec77f001f0f52492ce902ee6475e36803
iJoyRide <sumfeis3@gmail.com>
2024-01-03 23:29:10 +0800

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     images:{
         domains:["firebasestorage.googleapis.com"],
 
     },
-};
+}
 
-module.exports = nextConfig;
+
