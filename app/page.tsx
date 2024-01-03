3eb5eebe4b128f31c930f70e29f853a9ebea4769
iJoyRide <sumfeis3@gmail.com>
2024-01-03 17:49:01 +0800

    banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
 
 export default function 
 Home() {
-  return (
-    <div> Body</div>
+  return (<div>
+    <Container>
+      <div className="p-8">
+        <HomeBanner/>
+      </div>
+    </Container>
+
+    </div>
   )
 }
