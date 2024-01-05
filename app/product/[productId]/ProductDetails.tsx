56e44cf327cec04488312fce2815c37ed06b1635
iJoyRide <sumfeis3@gmail.com>
2024-01-05 15:33:04 +0800

    colour of phone

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import SetColor from "@/app/components/products/SetColor";
 import { Rating } from "@mui/material";
-import { use, useState } from "react";
+import { use, useCallback, useState } from "react";
 
 interface ProductDetailsProps{
     product: any
@@ -44,9 +45,18 @@ const ProductDetails:React.FC<ProductDetailsProps> =
 
     });
 
+    console.log(cartProduct)
+
     const productRating = 
         product.reviews.reduce((acc: number, item: any) => 
         item.rating + acc, 0) / product.reviews.length;
+
+    const handleColorSelect = useCallback((value: SelectedImgType) => {
+        setCartProduct((prev) => {
+            return {...prev, selectedImg: value}
+        })
+    }, 
+    [cartProduct.selectedImg])
 
     return <div className="grid grid-cols-1
     md:grid-cols-2 gap-12">
@@ -75,7 +85,11 @@ const ProductDetails:React.FC<ProductDetailsProps> =
             <div className={product.inStock ? "text-teal-400" : "text-rose-400"}>
                 {product.inStock ? "In stock" : "Out of stock"}</div>
             <Horizontal/>
-            <div>color</div>
+            <SetColor
+            cartProduct={cartProduct}
+            images={product.images}
+            handleColorSelect={handleColorSelect}
+            />
             <Horizontal/>
             <div>quantity</div>
             <Horizontal/>
