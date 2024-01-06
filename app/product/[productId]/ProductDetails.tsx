83c705055f5d497e51ac8f6305abc30c7dc06023
iJoyRide <sumfeis3@gmail.com>
2024-01-07 04:54:30 +0800

    larger image of product

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/app/components/products/Button";
+import ProductImage from "@/app/components/products/ProductImages";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
@@ -53,7 +54,7 @@ const ProductDetails:React.FC<ProductDetailsProps> =
         item.rating + acc, 0) / product.reviews.length;
 
     const handleColorSelect = useCallback((value: SelectedImgType) => {
-        if (cartProduct.quantity == 99){
+        if (cartProduct.quantity === 99){
             return;
         }
         
@@ -82,7 +83,11 @@ const ProductDetails:React.FC<ProductDetailsProps> =
 
     return <div className="grid grid-cols-1
     md:grid-cols-2 gap-12">
-        <div>Images</div>
+        <ProductImage 
+        cartProduct={cartProduct} 
+        product={product} 
+        handleColorSelect={handleColorSelect}/>
+
         <div className="flex flex-col gap-1 text-slate-500 text-sm">
             <h2 className="text-3xl font-medium text-slate-700">
                 {product.name}
