932480471aad8c8e843053ed4a448ec0d1ffb7a0
iJoyRide <sumfeis3@gmail.com>
2024-01-09 22:43:49 +0800

    checkout cart

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -4,6 +4,8 @@ import { useCart } from "@/hooks/useCarts";
 import Link from "next/link";
 import { MdArrowBack } from "react-icons/md";
 import Heading from "../components/Heading";
+import Button from "../components/products/Button";
+
 
 const CartClient = () => {
     const {cartProducts} = useCart()
@@ -32,7 +34,33 @@ const CartClient = () => {
             <div className="justify-self-center">QUANTITY</div>
             <div className="justify-self-end">TOTAL</div>
         </div>
-    </div>;
+        <div>
+            {cartProducts && cartProducts.map((item) => {
+                return <div key= {item.id}>{item.name}</div>
+            })}
+        </div>
+        <div className=" border-t-[1.5px] border-slate-200 py-4 flex justify-between gap-4">
+            <div className="w-[90px]">
+                <Button label="Clear Cart" onClick={() => {}} small outline/>
+            </div>
+            <div className="text-sm flex flex-col gap-1 items-start">
+                <div className="flex justify-between w-full text-base font-semibold">
+                    <span>Subtotal</span>
+                    <span></span>                        
+                </div>
+                <p className="text-slate-500">Taxes and shipping calculate at checkout</p>
+                <Button label="Chekout" onClick={() => {}}/>
+                
+                <Link href={"/"} className="
+                text-slate-500 flex items-center gap-1 
+                    mt-2
+                ">
+                    <MdArrowBack/>
+                    <span>Continue Shopping</span>
+                </Link>                         
+            </div>
+        </div>
+    </div>
 };
  
 export default CartClient;
