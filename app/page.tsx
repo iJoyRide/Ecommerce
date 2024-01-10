ad9386df71d41774e41aeb0aefd733e0305a1ddd
iJoyRide <sumfeis3@gmail.com>
2024-01-10 12:37:06 +0800

    adjusdted the grid columns

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,13 @@ export default function Home() {
       <div className="p-8">
         <HomeBanner/>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 
-      lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {products.map((product: any) => {
-          return <ProductCard key={product.id} data={product}/>;
-        })}
+      <div className="flex justify-center">
+        <div className="grid grid-cols-2 sm:grid-cols-3 
+        lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
+          {products.map((product: any) => {
+            return <ProductCard key={product.id} data={product}/>;
+          })}
+        </div>
       </div>
     </Container>;
 
