92cfcee8098baa2e2b3591d5f00c8224759d3eeb
iJoyRide <sumfeis3@gmail.com>
2024-01-13 10:32:55 +0800

    set up mongo db, api, register

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,11 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/products/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import {toast} from "react-hot-toast";
+import {signIn} from 'next-auth/react'
+import { useRouter } from "next/navigation";
+
 
 const RegisterForm = () => {
 
@@ -19,11 +24,36 @@ const RegisterForm = () => {
         }
     });
 
+    const router = useRouter()
+
     const onSubmit: SubmitHandler<FieldValues> = (data) => 
     {
-        setIsLoading(true)
-        console.log(data)
-    }
+        setIsLoading(true);
+        
+        axios.post('/api/register', data).then(() => {
+            toast.success('Account created successfully')
+            
+            signIn("credentials", {
+                email: data.email,
+                password: data.password,
+                redirect: false,
+            }).then((callback) => {
+                if(callback?.ok){
+                    router.push('/cart');
+                    router.refresh(); 
+                    toast.success("Logged In");
+                }
+
+                if(callback?.error){
+                    toast.error(callback.error)
+                }
+
+            });
+            
+        }).catch(() => toast.error("Something went wrong")).finally(() =>{
+            setIsLoading(false);
+        });
+    };
 
     return ( 
         <>
