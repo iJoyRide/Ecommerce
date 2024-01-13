fa21c33068677beb69df0d38df4ecc5f6d7e2690
iJoyRide <sumfeis3@gmail.com>
2024-01-13 17:54:03 +0800

    change the price

--- utils/products.tsx
+++ utils/products.tsx
@@ -69,7 +69,7 @@ export const products = [
       name: "Apple iPhone 12, 64GB",
       description:
         'The product is refurbished, fully functional, and in excellent condition. Backed by the 90-day E~Shop Renewed Guarantee.\n- This pre-owned product has been professionally inspected, tested and cleaned by Amazon qualified vendors. It is not certified by Apple.\n- This product is in "Excellent condition". The screen and body show no signs of cosmetic damage visible from 12 inches away.\n- This product will have a battery that exceeds 80% capacity relative to new.\n- Accessories may not be original, but will be compatible and fully functional. Product may come in generic box.\n- Product will come with a SIM removal tool, a charger and a charging cable. Headphone and SIM card are not included.\n- This product is eligible for a replacement or refund within 90-day of receipt if it does not work as expected.\n- Refurbished phones are not guaranteed to be waterproof.',
-      price: 40,
+      price: 1999,
       brand: "Apple",
       category: "Phone",
       inStock: true,
@@ -142,7 +142,7 @@ export const products = [
       name: "Logitech MX Master 2S Wireless Mouse – Use on Any Surface, Hyper-Fast Scrolling, Ergonomic Shape, Rechargeable, Control Upto 3 Apple Mac and Windows Computers, Graphite",
       description:
         "Cross computer control: Game changing capacity to navigate seamlessly on 3 computers, and copy paste text, images, and files from 1 to the other using Logitech flow\nDual connectivity: Use with upto 3 Windows or Mac computers via included Unifying receiver or Bluetooth Smart wireless technology. Gesture button- Yes",
-      price: 70,
+      price: 75,
       brand: "logitech",
       category: "Accesories",
       inStock: true,
@@ -162,7 +162,7 @@ export const products = [
       name: 'Smart Watch(Answer/Make Call), 1.85" Smartwatch for Men Women IP68 Waterproof, 100+ Sport Modes, Fitness Activity Tracker, Heart Rate Sleep Monitor, Pedometer, Smart Watches for Android iOS, 2023',
       description:
         'Bluetooth Call and Message Reminder: The smart watch is equipped with HD speaker, after connecting to your phone via Bluetooth, you can directly use the smartwatches to answer or make calls, read messages, store contacts, view call history. The smartwatch can set up more message notifications in "GloryFit" APP. You will never miss any calls and messages during meetings, workout and riding.',
-      price: 50,
+      price: 699,
       brand: "Nerunsa",
       category: "Watch",
       inStock: true,
